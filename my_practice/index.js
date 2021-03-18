be553330545f761ba91684a2677a75144fb9dd1e
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-03-18 08:08:40 +0530

    anagrams&regex

--- my_practice/index.js
+++ my_practice/index.js
@@ -1,36 +1,36 @@
 // // --- Directions
-// // Given an integer, return an integer that is the reverse
-// // ordering of numbers.
-// // --- Examples
+// // // Given an integer, return an integer that is the reverse
+// // // ordering of numbers.
+// // // --- Examples
 // //   reverseInt(15) === 51
 // //   reverseInt(981) === 189
 // //   reverseInt(500) === 5
 // //   reverseInt(-15) === -51
 // //   reverseInt(-90) === -9
 
-// // function reverseInt(numbers) {
+// function reverseInt(numbers) {
 
-// // 	return numbers.split('').reverse().join('');
+// 	return numbers.split('').reverse().join('');
 
-// // }
+// }
 
-// // reverseInt(15);
+// reverseInt(15);
 
-// // module.exports = reverseInt;
+// module.exports = reverseInt;
 
-// // function reverseInt(word) {
+// function reverseInt(word) {
 
-// // 	return numbers.split('').reverse().join('');
+// 	return numbers.split('').reverse().join('');
 
-// // }
+// }
 
-// // reverseInt('apple');
+// reverseInt('apple');
 
-// // module.exports = reverseInt;
+// module.exports = reverseInt;
 
 // function reverseChar(word){
 
-// 	// word.forEach((letter)=>console.log(letter));
+// 	word.forEach((letter)=>console.log(letter));
 
 //     let reversed = '';
 //     for(let letter of word){
@@ -346,22 +346,44 @@
 // 	}
 // }
 
-let output = chunk([1, 2, 3, 4], 2);
-console.log(output);
+// ///Better solution///
+// let output = chunk([1, 2, 3, 4], 2);
+// console.log(output);
 
-function chunk(array, size) {
-	let arr = [];
-	while (array.length > 0) {
-		arr.push(array.slice(0, size));
-	}
+// function chunk(array, size) {
+// 	let arr = [];
+// 	while (array.length > 0) {
+// 		arr.push(array.slice(0, size));
+// 	}
 
-	return arr;
+// 	return arr;
+// }
+
+///Anagrams  = a=hello b=olleh => no .of letter should be same and avoid extra space and non word
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+anagrams('rail safety', 'fairy tales');
+function anagrams(stringA, stringB) {
+	let Astring = buildCharMap(stringA);
+	let BString = buildCharMap(stringB);
 }
 
-// let op = splic([1,2,3])
-// console.log(op);
-// function splic(arr){
-//    console.log(arr.splice(0,2)); arr.slice(0,2);
-//     return arr
+function buildCharMap(str) {
+	let charMap = {};
 
-// }
+	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+	return charMap;
+}
+
+let output = buildCharMap('hello');
+console.log(output);
