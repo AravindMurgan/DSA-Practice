8e6867eb5bbc7dd513dce0861cc1d81886be9131
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-03-30 14:50:11 +0530

    rec-sol

--- completed_exercises/steps/index.js
+++ completed_exercises/steps/index.js
@@ -18,33 +18,65 @@
 //       '####'
 
 function steps(n, row = 0, stair = '') {
-  if (n === row) {
-    return;
-  }
+	if (n === row) {
+		return;
+	}
 
-  if (n === stair.length) {
-    console.log(stair);
-    return steps(n, row + 1);
-  }
+	if (n === stair.length) {
+		console.log(stair);
+		return steps(n, row + 1);
+	}
 
-  const add = stair.length <= row ? '#' : ' ';
-  steps(n, row, stair + add);
+	const add = stair.length <= row ? '#' : ' ';
+	steps(n, row, stair + add);
 }
 
 module.exports = steps;
 
-// function steps(n) {
-//   for (let row = 0; row < n; row++) {
-//     let stair = '';
-//
-//     for (let column = 0; column < n; column++) {
-//       if (column <= row) {
-//         stair += '#';
-//       } else {
-//         stair += ' ';
-//       }
-//     }
-//
-//     console.log(stair);
-//   }
-// }
+function steps(n) {
+	for (let row = 0; row < n; row++) {
+		let stair = '';
+
+		for (let column = 0; column < n; column++) {
+			if (column <= row) {
+				stair += '#';
+			} else {
+				stair += ' ';
+			}
+		}
+
+		console.log(stair);
+	}
+}
+console.log(steps(3));
+
+function printingSteps(steps) {
+	debugger;
+	for (let row = 0; row < steps; row++) {
+		let stair = '';
+
+		for (let column = 0; column < steps; column++) {
+			if (column <= row) {
+				stair += '#';
+			} else {
+				stair += ' ';
+			}
+		
+		}
+    console.log(stair);
+	}
+}
+
+console.log(printingSteps(3));
+
+
+//recursive solution//
+
+function recursiveSteps(n){
+
+  //base-case//
+  
+
+}
+
+recursiveSteps(3)
