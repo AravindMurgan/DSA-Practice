9cf9c57b800b2cdaa2c0ebe6630d3511e72b8f09
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-05 17:32:53 +0530

    pracitce-LL

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -14,7 +14,15 @@
 
 function midpoint(list) {
 
-    
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while(fast.next && fast.next.next){
+        slow = list.next;
+        fast = list.next.next
+    }
+
+    return slow
 
 
 }
