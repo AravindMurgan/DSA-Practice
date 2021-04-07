c6f0b33ff1f9c9a325ebda3de2f7b159acbeb588
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-07 11:04:20 +0530

    fromLast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,27 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    debugger
+
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while(n>0){
+        fast = fast.next;
+        n--;
+    }
+
+    while(fast.next){
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+
+
+}
+
+fromLast(1,3);
 
 module.exports = fromLast;
