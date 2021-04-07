720e44459d26e586e8afcac7386e2915afb6cdf5
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-07 13:09:41 +0530

    Trees

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,31 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
 
-class Tree {}
+    constructor(data,){
+        this.data = data;
+        this.children  = [];
+
+    }
+
+    add(data){
+        this.children.push(new Node(data))
+    }
+
+    remove(data){
+
+        while()
+    }
+
+}
+
+class Tree {
+
+    constructor(){
+        this.root = null;
+    }
+
+}
 
 module.exports = { Tree, Node };
