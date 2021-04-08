5cde9fa560ba1a673d3506db265544eebf6b98d9
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-08 21:18:53 +0530

    tree

--- exercises/levelwidth/node.js
+++ exercises/levelwidth/node.js
@@ -1,10 +1,59 @@
-module.exports = class Node {
-  constructor(data) {
-    this.data = data;
-    this.children = [];
-  }
+// module.exports = class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.children = [];
+//   }
 
-  add(data) {
-    this.children.push(new Node(data));
-  }
-};
+//   add(data) {
+//     this.children.push(new Node(data));
+//   }
+// };
+
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.children = [];
+		console.log(this.children);
+	}
+
+	add(data) {
+		return this.children.push(new Node(data));
+	}
+
+	remove(data) {
+		this.children.filter((node) => {
+			return node.data !== data;
+		});
+	}
+}
+
+const newNode = new Node();
+
+class Tree {
+	constructor() {
+		this.root = null;
+	}
+
+	traverseBF(fn) {
+		const arr = [this.root];
+
+		while (arr.length) {
+			const node = arr.shift();
+			arr.push(...node.children);
+			fn(node);
+		}
+	}
+
+}
+
+const letters = [];
+const t = new Tree();
+t.root = new Node('a');
+t.root.add('b');
+t.root.add('c');
+t.root.children[0].add('d');
+
+t.traverseBF((node) => {
+	letters.push(node.data);
+
+});
