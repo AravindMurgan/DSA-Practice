37744bdb4591450f33ae0bd989ab565281be762c
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-11 10:50:41 +0530

    binary-tree

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -33,6 +33,21 @@ class Node {
 
     }
 
+    contains(data){
+
+        if(data === this.data){
+            return this
+        }
+
+        if(data > this.data && this.right){
+            return this.right.contains(data);
+        }else if(data < this.data && this.left){
+            return this.left.contains(data)
+        }
+
+        return null;
+    }
+
   
 }
 
