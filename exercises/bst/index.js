323a254953f6f28160b4bec5a6d7ececff4cd484
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-11 10:53:47 +0530

    contains

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -51,4 +51,15 @@ class Node {
   
 }
 
+const node = new Node(10);
+  node.insert(5);
+  node.insert(15);
+  node.insert(20);
+  node.insert(0);
+  node.insert(-5);
+  node.insert(3);
+
+  node.contains(3);
+  console.log(object);
+
 module.exports = Node;
