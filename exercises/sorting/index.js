c8cb625f5952da79792a08eedf0b6a2566a2107b
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-12 16:39:18 +0530

    refactoring

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,48 +1,54 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(nums) {
+function bubbleSort(arr) {
 
-    // let length = arr.length;
+	let length = arr.length;
 
-    // for(let i =0 ; i < length; i++){
+	// for (let i = 0; i < length; i++) {
+	// 	for (let j = 0; j < length - i - 1; j++) {
+	// 		if (arr[j] > arr[j + 1]) {
+	// 			const less = arr[j + 1];
 
-    //     for(let j=0; j <= length - i ; j++){
-    //         if(j > j+1){
-    //             j = j+1;
-    //             j+1 = j;
-    //         }
-    //     }
-    //     return 
+	// 			arr[j + 1] = arr[j];
+	// 			arr[j] = less;
+	// 		}
+	// 	}
+	// }
 
-    // }
+    for(let i =0; i< length - 1; i++){
+        if(arr[i] > arr[i+1] ){
+            const less = arr[i+1];
 
-    let i =0;
-    while( i < nums.length){
-        const j = nums[i] -1;
-
-        if(nums[i] !== nums[j]){
-            [nums[i],nums[j]]= [nums[j],nums[i]]
-        }else{
-            i +=1;
+            arr[i+1] = arr[i];
+            arr[i] = less
         }
-
     }
-    return nums
 
 
+	return arr;
 }
 
-function selectionSort(arr) {
+bubbleSort([-60,95,0,20,10]);
 
-}
 
-function mergeSort(arr) {
+// let i =0;
+// while( i < nums.length){
+//     const j = nums[i] -1;
 
-}
+//     if(nums[i] !== nums[j]){
+//         [nums[i],nums[j]]= [nums[j],nums[i]]
+//     }else{
+//         i +=1;
+//     }
 
-function merge(left, right) {
+// }
+// return nums
 
-}
+function selectionSort(arr) {}
+
+function mergeSort(arr) {}
+
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
