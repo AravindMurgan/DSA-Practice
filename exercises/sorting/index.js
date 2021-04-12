cc5f97667f053c1260606c06e3b9d220d5435106
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-12 15:33:36 +0530

    bubble-sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,6 +3,20 @@
 
 function bubbleSort(arr) {
 
+    let length = arr.length;
+
+    for(let i =0 ; i < length; i++){
+
+        for(let j=0; i <= length - i ; j++){
+            if(j > j+1){
+                j = j+1;
+                j+1 = j;
+            }
+        }
+
+    }
+
+
 }
 
 function selectionSort(arr) {
