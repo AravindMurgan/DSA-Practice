8b02e5843964c4b3dec3a76dc8c10c3ee63d3b03
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-12 16:16:01 +0530

    cyclic-sort --approach

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,21 +1,34 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+function bubbleSort(nums) {
 
-    let length = arr.length;
+    // let length = arr.length;
 
-    for(let i =0 ; i < length; i++){
+    // for(let i =0 ; i < length; i++){
 
-        for(let j=0; j <= length - i ; j++){
-            if(j > j+1){
-                j = j+1;
-                j+1 = j;
-            }
+    //     for(let j=0; j <= length - i ; j++){
+    //         if(j > j+1){
+    //             j = j+1;
+    //             j+1 = j;
+    //         }
+    //     }
+    //     return 
+
+    // }
+
+    let i =0;
+    while( i < nums.length){
+        const j = nums[i] -1;
+
+        if(nums[i] !== nums[j]){
+            [nums[i],nums[j]]= [nums[j],nums[i]]
+        }else{
+            i +=1;
         }
-        return 
 
     }
+    return nums
 
 
 }
