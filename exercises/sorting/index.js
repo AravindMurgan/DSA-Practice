1124badbe5a340413ed2ca137725d231a90b328c
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-13 20:11:00 +0530

    selection-sort & debug

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -42,7 +42,26 @@ console.log(bubbleSort([-40, +100, -124, -40, 0, +21, 7, -21, -100, 500]));
 // }
 // return nums
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+
+	for(let i = 0 ; i < arr.length ; i++){
+		let indexofMin = i;
+
+		for(let j = i+1; j<arr.length ; i++ ){
+			if(arr[j] < arr[indexofMin] ){
+				indexofMin = j;
+			}
+		}
+
+	}
+
+	if(indexofMin !== i){
+		let lesser = arr[indexofMin];
+		arr[indexofMin] = arr[i];
+		arr[i] = lesser;
+	}
+
+}
 
 function mergeSort(arr) {}
 
