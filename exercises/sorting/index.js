750d0ae93a005630788ddccacf47b311d10e8385
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-14 19:43:48 +0530

    debug

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -4,43 +4,20 @@
 function bubbleSort(arr) {
 	let length = arr.length;
 
-	for (let i = 0; i < length; i++) {
-		for (let j = 0; j < length - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				const less = arr[j + 1];
+	for (let i = 0; i < length - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+			const less = arr[i + 1];
 
-				arr[j + 1] = arr[j];
-				arr[j] = less;
-			}
+			arr[i + 1] = arr[i];
+			arr[i] = less;
 		}
 	}
-
-	// for (let i = 0; i < length - 1; i++) {
-	// 	if (arr[i] > arr[i + 1]) {
-	// 		const less = arr[i + 1];
-
-	// 		arr[i + 1] = arr[i];
-	// 		arr[i] = less;
-	// 	}
-	// }
 
 	return arr;
 }
 
 console.log(bubbleSort([-40, +100, -124, -40, 0, +21, 7, -21, -100, 500]));
 
-// let i =0;
-// while( i < nums.length){
-//     const j = nums[i] -1;
-
-//     if(nums[i] !== nums[j]){
-//         [nums[i],nums[j]]= [nums[j],nums[i]]
-//     }else{
-//         i +=1;
-//     }
-
-// }
-// return nums
 
 function selectionSort(arr) {
 
@@ -53,20 +30,22 @@ function selectionSort(arr) {
 			}
 		}
 
+		if(indexofMin !== i){
+			let lesser = arr[indexofMin];
+			arr[indexofMin] = arr[i];
+			arr[i] = lesser;
+		}
+
 	}
 
-	if(indexofMin !== i){
-		let lesser = arr[indexofMin];
-		arr[indexofMin] = arr[i];
-		arr[i] = lesser;
-	}
+	
 
 }
 
 selectionSort([9,4,6,0,1,3]);
 
-function mergeSort(arr) {}
+// function mergeSort(arr) {}
 
-function merge(left, right) {}
+// function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
