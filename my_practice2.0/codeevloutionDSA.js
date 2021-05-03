d5b818418ab108bac55019294cd6e59a94b02852
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-03 06:21:50 +0530

    JsConcepts

--- my_practice2.0/codeevloutionDSA.js
+++ my_practice2.0/codeevloutionDSA.js
@@ -31,7 +31,7 @@ twoSums(nums,9)
 */
 
 /* Factorial Number */
-
+/*
 function factorial(n) {
 	let answer = 1;
 	if (n === 0 || n === 1) {
@@ -49,3 +49,110 @@ factorial(4);
 
 const myapp = 'adasd';
 console.log(myapp);
+*/
+
+/*
+
+
+/* Pattern - Sliding Window 27/04/2021
+
+function findAverageofSubArray(k,arr){
+  const result=[];
+  for(leti=0; i<arr.length - k+1; i++){
+    sum=0.0;
+  for(letj=i; j< i + k ; j++ ){
+    sum+=arr[j]
+  }
+    result.push(sum/k);
+  }
+  return result
+} 
+
+ const answer = findAverageofSubArray(5,[1,3,2,6,-1,2])
+ console.log(answer)
+/*
+
+/*Slidiing Window Pattern*/
+/*
+function findAverageofSubArray(k,arr){
+  const result=[];
+  let windowSum=0;
+  let windowStart=0;
+  let length = arr.length
+  for(let windowEnd = 0; windowEnd < length; windowEnd++){
+    windowsSum += arr[windowEnd];
+    
+    if(windowEnd >= k-1;){
+      result.push(windowSum / k);
+      windowSum -= arr[windowStart];
+      windowStart +=1;
+    }
+  }
+  return result
+}
+
+const answer = findAverageofSubArray(5,[1,3,2,6,-1]);
+
+*/
+
+/*3. Find smallest sub array with value greater or value to S
+
+i/p: [1,2,3,4,5] s=9 ;  o/p:[4,5] 
+
+*/
+
+
+function smallestSubArray(arr,s){
+  debugger
+  let windowSum=0;
+  let windowStart=0;
+  let minLength= Infinity;
+  let length = arr.length;
+  for(let windowEnd=0; windowEnd < length; windowEnd++){
+    windowSum += arr[windowEnd]
+    
+    while(windowSum >= s){
+      minLength = Math.min(minLength,windowEnd - windowStart + 1);
+      windowSum -= arr[windowStart];
+      windowStart +=1;
+    }
+    
+  }
+  
+  if(minLength === Infinity){
+    return 0;
+  }
+  
+  return minLength
+  
+}
+
+const answer =  smallestSubArray([1,2,3,4,5],9);
+
+
+let minlen = Math.min(Infinity,3-0 + 1);
+console.log(minlen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
