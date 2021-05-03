f263a8146ea15917672ffacba29f09a57b440ac7
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-03 11:41:27 +0530

    closures with memoization

---
+++ my_practice2.0/JSconcepts.js
@@ -0,0 +1,116 @@
+/* 3/5/2021  -- Scope and Closures */
+/*
+function outer(){
+  let counter=0;
+  function inner(){
+    counter++
+    console.log(counter)
+  }
+  inner()
+};
+
+outer()
+
+*/
+/*
+function outer(){
+  let counter=0;
+  
+  function inner(){
+    counter ++
+    console.log(counter)
+  }
+  return inner
+}
+
+let fn = outer()
+fn()
+fn()
+fn()
+
+let fn2= outer()
+fn2()
+fn2()
+
+*/
+
+function square(num){
+  
+  return num*num
+}
+
+
+function Memoization(){
+  let cache = {}
+  	
+  return function optimizeSquare(num){
+    if(num in cache){
+      
+      return cache[num]
+    }else{
+      const result = square(num);
+      cache[num]= result
+      
+      return result
+    }
+    
+  }
+  
+}
+
+const memoizeSquare = Memoization();
+console.log(memoizeSquare(2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
