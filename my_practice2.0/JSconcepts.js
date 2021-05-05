02e3d2cec6320f4579febf0f711f29f29d20bcb3
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-05 09:38:41 +0530

    class-ES6

--- my_practice2.0/JSconcepts.js
+++ my_practice2.0/JSconcepts.js
@@ -163,6 +163,48 @@ jhondoe.getMyName()
 
 /*5/5/2021  JS-Concepts  **************************************/
 
+/* ES6 - Class Methods   */
+
+class Calculator{
+  constructor(){
+    this.value=0;
+   
+  }
+  
+  add(num){
+    this.value += num
+     return this
+  }
+  
+  sub(num){
+    this.value -= num
+    return this
+  }
+  
+  print(){
+    console.log(this.value)
+  }
+}
+
+class ScientificCalculator extends Calculator {
+  	square(){
+      this.value *= this.value
+    }
+}
+
+const s = new ScientificCalculator();
+
+s.add(10).sub(5).print().square()
+
+
+
+
+
+
+
+
+
+
 
 
 
