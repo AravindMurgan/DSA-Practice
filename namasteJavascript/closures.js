af4781934d208ae3e9a09f3ce32423006407650e
AravindMurugan <mi.arvindmurugan@gmail.com>
2022-01-16 14:24:28 +0530

    closures_inDepth

--- namasteJavascript/closures.js
+++ namasteJavascript/closures.js
@@ -10,23 +10,47 @@
 // }
 // a();
 
-function c() {
-    let y = 55;
-    function d() {
-        function a() {
-            let x = ['hello', 'hello2', 'hello3'];
-            console.log(z);
-            function b() {
-                let z = 25;
-                x.map(item => console.log(item, y));
-            }
-            x = ['test', 'test2', 'test3'];
-            b();
-            return b
-        }
-        a();
+// function c() {
+//     let y = 55;
+//     function d() {
+//         function a() {
+//             let x = ['hello', 'hello2', 'hello3'];
+//             console.log(z);
+//             function b() {
+//                 let z = 25;
+//                 x.map(item => console.log(item, y));
+//             }
+//             x = ['test', 'test2', 'test3'];
+//             b();
+//             return b
+//         }
+//         a();
+//     }
+//     d();
+// }
+
+// c();
+
+
+function Counter() {
+    let count = 0;
+    this.incrementCounter = function () {
+        count++;
+        console.log(count);
     }
-    d();
+
+    this.decrementCounter = function () {
+        count--;
+        console.log(count);
+    }
+
 }
 
-c();
+let counter1 = new Counter;
+
+counter1.incrementCounter();
+counter1.incrementCounter();
+counter1.incrementCounter();
+
+counter1.decrementCounter();
+counter1.decrementCounter();
