c8a7fff0f35fa138c077e01832c17eafc0519d42
AravindMurugan <mi.arvindmurugan@gmail.com>
2022-02-03 23:13:48 +0530

    lengthOFLastWord

--- leetcode2022/lengthOfLastWord.js
+++ leetcode2022/lengthOfLastWord.js
@@ -16,3 +16,35 @@ var lengthOfLastWord = function (s) {
 // }
 
 // console.log(lengthOfLastWord("   fly me   to   the moon  "));
+
+
+var addBinary = function (a, b) {
+    let sum;
+    let carry = 0;
+
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    while (i >= 0 || j >= 0) {
+        let A = a[i] ? a[i] : 0
+        let B = b[j] ? b[j] : 0
+
+        sum = String(A ^ B ^ carry) + sum;
+
+        if (a === b && a !== carry) {
+            carry = Number(!carry)
+        }
+
+        i--;
+        j--;
+    }
+
+    if (carry) {
+        // sum = (carry + sum).toString()
+        sum = String(carry) + sum
+    }
+
+    return sum;
+}
+
+console.log(addBinary('11', '1'));
