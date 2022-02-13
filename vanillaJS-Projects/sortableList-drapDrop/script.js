c92cacc93b777f55c3de7f142b062efd769afdc8
AravindMurugan <mi.arvindmurugan@gmail.com>
2022-02-13 19:56:30 +0530

    inital-setup

---
+++ vanillaJS-Projects/sortableList-drapDrop/script.js
@@ -0,0 +1,6 @@
+const draggableList = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = ['Jeff Bezos', 'Bill Gates', 'Warren Buffet',
+    'Bernard Arnault', 'Carlos Slim Helu', 'Amancio Ortega', 'Larry Ellison', 'Mark Zuckerberg', 'Micheal Bloomberg', 'Larry Page'];
+
