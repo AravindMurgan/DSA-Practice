013839fb75f92a5283a0c1fc3479ce39880ecf4a
AravindMurugan <mi.arvindmurugan@gmail.com>
2022-03-09 22:44:12 +0530

    binarySearchTree

---
+++ interviewPreparation/binarySearchTree/binarySearchTree.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const depthFirstValues = (root) => {
+    const result = []
+    const stack = [root]
+    while (stack.length > 0) {
+        const current = stack.pop()
+        result.push(current.val)
+
+        if (current.right) stack.push(current.right)
+        if (current.left) stack.push(current.left)
+    }
+
+    return result
+}
+
+
+const A = new Node('A');
+const B = new Node('B');
+const C = new Node('C');
+const D = new Node('D');
+const E = new Node('E');
+const F = new Node('F');
+
+A.left = B
+A.right = C
+B.left = D
+B.right = E
+C.left = F
+depthFirstValues(A)
+
+
---
+++ interviewPreparation/march-22/sortArray012.js
@@ -0,0 +1,8 @@
+function sortArray(arr, n) {
+    let array = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            array.push()
+        }
+    }
+}
