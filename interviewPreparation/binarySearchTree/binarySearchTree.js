031bbfe3e703bcff008d4718974f70b9eeaa66c0
AravindMurugan <mi.arvindmurugan@gmail.com>
2022-04-15 14:44:47 +0530

    starting DSA.. after a break

--- interviewPreparation/binarySearchTree/binarySearchTree.js
+++ interviewPreparation/binarySearchTree/binarySearchTree.js
@@ -7,18 +7,28 @@ class Node {
 }
 
 const depthFirstValues = (root) => {
-    const result = []
-    const stack = [root]
-    while (stack.length > 0) {
-        const current = stack.pop()
-        result.push(current.val)
 
-        if (current.right) stack.push(current.right)
-        if (current.left) stack.push(current.left)
-    }
+    if (root === null) return [];
+    const leftValues = depthFirstValues(root.left);
+    const rightValues = depthFirstValues(root.right);
 
-    return result
+    return [root.val, ...leftValues, ...rightValues];
 }
+
+
+// const depthFirstValues = (root) => {
+//     const result = []
+//     const stack = [root]
+//     while (stack.length > 0) {
+//         const current = stack.pop()
+//         result.push(current.val)
+
+//         if (current.right) stack.push(current.right)
+//         if (current.left) stack.push(current.left)
+//     }
+
+//     return result
+// }
 
 
 const A = new Node('A');
