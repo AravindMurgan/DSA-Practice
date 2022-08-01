1348b2e48b39aa4e065a2a74a072e8230d5e56ff
AravindMurugan <mi.arvindmurugan@gmail.com>
2022-08-01 23:59:01 +0530

    quick sort

--- DSA-codeEvolution/algos.js
+++ DSA-codeEvolution/algos.js
@@ -180,4 +180,27 @@ function bubbleSort(arr) {
     return arr
 }
 
-console.log('bubbleSort:::', bubbleSort([8, 20, -2, 4, - 6])) 
+console.log('bubbleSort:::', bubbleSort([8, 20, -2, 4, - 6]))
+
+//27/2/2022
+//i/p:[8,20,-2,4,-6] o/p:[-6,-2,4,8,20]
+function quickSort(arr) {
+    //baseCase
+    if (arr.length < 2) {
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort([8, 20, -2, 4, -6]))
