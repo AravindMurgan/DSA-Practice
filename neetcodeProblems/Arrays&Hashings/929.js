5701043294751b0065d5da14d78358244d089e1e
AravindMurugan <mi.arvindmurugan@gmail.com>
2023-07-05 08:27:11 +0530

    929

--- neetcodeProblems/Arrays&Hashings/929.js
+++ neetcodeProblems/Arrays&Hashings/929.js
@@ -35,6 +35,32 @@ var numUniqueEmails = function (emails) {
     return [...result].length
 }
 
-emails = ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]
+// const isValidEmail = (email) => {
+//     let arr = email.split('@')
+//     let count = 0
+//     for (let char of arr[1]) {
+//         if (char == '.') {
+//             count += 1
+//         }
+//         if (char == '+') {
+//             count += 1
+//         }
+//     }
+
+//     return count > 1 ? 0 : 1
+// }
+
+// var numUniqueEmails = function (emails) {
+//     let result = 0
+//     for (let email of emails) {
+//         let output = isValidEmail(email)
+
+//         result += output
+//     }
+
+//     return result
+// };
+
+emails = ["fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "fg.r.u.uzj+o.f.d@kziczvh.com", "r.cyo.g+ng.r.iq@tgsg.z.com", "fg.r.u.uzj+lp.k@kziczvh.com", "r.cyo.g+n.h.e+n.g@tgsg.z.com", "fg.r.u.uzj+k+p.j@kziczvh.com", "fg.r.u.uzj+w.y+b@kziczvh.com", "r.cyo.g+x+d.c+f.t@tgsg.z.com", "r.cyo.g+x+t.y.l.i@tgsg.z.com", "r.cyo.g+brxxi@tgsg.z.com", "r.cyo.g+z+dr.k.u@tgsg.z.com", "r.cyo.g+d+l.c.n+g@tgsg.z.com", "fg.r.u.uzj+vq.o@kziczvh.com", "fg.r.u.uzj+uzq@kziczvh.com", "fg.r.u.uzj+mvz@kziczvh.com", "fg.r.u.uzj+taj@kziczvh.com", "fg.r.u.uzj+fek@kziczvh.com"];
 
 numUniqueEmails(emails)
