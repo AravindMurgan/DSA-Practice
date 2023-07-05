25909dd49590dc23a8943f393e2eb53b98cb38c8
AravindMurugan <mi.arvindmurugan@gmail.com>
2023-07-05 08:25:42 +0530

    118

--- neetcodeProblems/Arrays&Hashings/118.js
+++ neetcodeProblems/Arrays&Hashings/118.js
@@ -1,25 +1,43 @@
+// var generate = function (numRows) {
+//     // var i = 0;
+//     // var j = 0;
+//     // Create an array list to store the output result...
+//     var res = [];
+//     // For generating each row of the triangle...
+//     for (i = 0; i < numRows; i++) {
+//         res.push(Array(i + 1));       // Initialize the first row of the pascal triangle as {1}...
+//         for (j = 0; j <= i; j++) {
+//             // Primary...
+//             if (j === 0 || j === i) {
+//                 res[i][j] = 1;
+//             }
+//             else {
+//                 // Calculate the elements of a row, add each pair of adjacent elements of the previous row in each step of the inner loop.
+//                 res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+
+//             }
+//         }
+//     }
+//     return res;      // Return the output list of pascal values...
+// };
+
 var generate = function (numRows) {
-    var i = 0;
-    var j = 0;
-    // Create an array list to store the output result...
-    var res = [];
-    // For generating each row of the triangle...
-    for (i = 0; i < numRows; i++) {
-        res.push(Array(i + 1));       // Initialize the first row of the pascal triangle as {1}...
-        for (j = 0; j <= i; j++) {
-            // Primary...
+    const result = []
+
+    for (let i = 0; i < numRows.length; ++i) {
+        result.push(Array(i + 1))
+
+        for (let j = 0; j <= i; ++j) {
             if (j === 0 || j === i) {
-                res[i][j] = 1;
-            }
-            else {
-                // Calculate the elements of a row, add each pair of adjacent elements of the previous row in each step of the inner loop.
-                res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+                result[i][j] = 1
+            } else {
+                result[i][j] = result[i - 1][j] + result[i - 1][j - 1]
             }
         }
     }
-    return res;      // Return the output list of pascal values...
+
+    return result
 };
 
-generate(5)
+console.log(generate(5))
 
-console.log(Array(5))
