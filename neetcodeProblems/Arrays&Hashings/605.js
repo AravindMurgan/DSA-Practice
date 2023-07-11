53f08ddac97f38732303996e7b31680cbd5604a0
AravindMurugan <mi.arvindmurugan@gmail.com>
2023-07-11 08:25:02 +0530

    605

---
+++ neetcodeProblems/Arrays&Hashings/605.js
@@ -0,0 +1,15 @@
+var canPlaceFlowers = function (flowerbed, n) {
+    for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+        if (
+            flowerbed[i] === 0 &&
+            flowerbed[i - 1] !== 1 &&
+            flowerbed[i + 1] !== 1
+        ) {
+            n--;
+            //   i++;
+        }
+    }
+    return n === 0;
+};
+
+canPlaceFlowers([1, 0, 0, 0, 0, 1], 2) //excepted::: false
