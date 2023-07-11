f550ccd06ac9ca432fc23f1ce676fbd3bf21ce59
AravindMurugan <mi.arvindmurugan@gmail.com>
2023-07-11 08:24:29 +0530

    2574

---
+++ neetcodeProblems/Arrays&Hashings/2574.js
@@ -0,0 +1,19 @@
+var leftRigthDifference = function (nums) {
+    let result = [];
+
+    let left = 0;
+    let right = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        right += nums[i];
+    }
+
+    for (var i = 0; i < nums.length; i++) {
+        right -= nums[i];
+        result.push(Math.abs(left - right));
+        left += nums[i];
+    }
+    return result;
+};
+
+leftRigthDifference([10, 4, 8, 3])
