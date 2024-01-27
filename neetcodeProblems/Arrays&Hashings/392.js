082cef6ecfc34247a1ae618875ac2c3651947547
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-01-27 11:55:12 +0000

    LC 392

--- neetcodeProblems/Arrays&Hashings/392.js
+++ neetcodeProblems/Arrays&Hashings/392.js
@@ -1,14 +1,32 @@
-var isSubsequence = function (s, t) {
-    let i = 0
-    let j = 0
+// var isSubsequence = function (s, t) {
+//     let i = 0
+//     let j = 0
 
-    while (i < s.length && j < t.length) {
-        if (s[i] === t[j]) i += 1
-        j += 1
+//     while (i < s.length && j < t.length) {
+//         if (s[i] === t[j]) i += 1
+//         j += 1
+//     }
+
+//     return i == s.length
+// };
+
+var isSubsequence = function(s, t) {
+    let str=s;
+    let i=0;
+    let j=0;
+
+    while(i < s.length){
+        if(s[i] == t[j]){
+            str= str.replace(s[i],'');
+            console.log(s);
+            i+=1
+        }
+        j+=1
     }
 
-    return i == s.length
+    return str.length ==0
 };
 
-s = "ab", t = "baab"
+
+s = "abc", t = "ahbgdc"
 isSubsequence(s, t)
